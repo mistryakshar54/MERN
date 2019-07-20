d22a23286188d7b68ac82f30e327b1687e81fc7c
Akshar Mistry <mistry.aks@gmail.com>
2019-07-20 14:46:03 +0530

    Add Components

---
+++ ShoppingCart/front-end/src/components/Layout/Content/Content.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AppContent = (props)=>{
+    return <h1>There comes the Content</h1>;
+
+}
+
+export default AppContent;
---
+++ ShoppingCart/front-end/src/components/Layout/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+const AppHeader = ( props )=>{
+    return (
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar.Brand href="#home">Shoppie</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto"></Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+}
+
+export default AppHeader;;
