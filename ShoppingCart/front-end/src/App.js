d3a5dee09b388c78284ba6af8db1655b209ce5f5
Akshar Mistry <mistry.aks@gmail.com>
2019-07-21 16:26:35 +0530

    Added sample images

--- ShoppingCart/front-end/src/App.js
+++ ShoppingCart/front-end/src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import AppHeader from "./components/Layout/Header/Header";
 import AppContent from "./components/Layout/Content/Content";
 import './App.scss';
+import store from "./store/store";
+import { Provider } from "react-redux";
 
 function App() {
   return (
-    <div className="App">
-      <AppHeader />
-      <AppContent />
-    </div>
+    <Provider store={store} >
+      <div className="App">
+        <AppHeader />
+        <AppContent />
+      </div>
+    </Provider>
   );
 }
 
