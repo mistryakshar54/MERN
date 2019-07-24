65605e108d55f0af9459bfe232396bb399c78c4e
Akshar Mistry <mistry.aks@gmail.com>
2019-07-24 23:00:26 +0530

    Experimental IO Dispatcher for future io requests

--- ShoppingCart/front-end/src/store/actioncreators/CoreActionCreators.js
+++ ShoppingCart/front-end/src/store/actioncreators/CoreActionCreators.js
@@ -1,3 +1,6 @@
 export const dispatchLoading = () => {};
 export const dispatchApiSuccess = () => {};
 export const dispatchApiError = () => {};
+export const dispatchPromise = ( endPoint , actionPayload ) => {
+    console.log("Got a dispatch for following" , endPoint , actionPayload);
+}
