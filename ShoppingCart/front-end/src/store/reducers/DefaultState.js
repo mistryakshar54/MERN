c62d6f526a82200b52858e5fa6c2cccec605c9ab
Akshar Mistry <mistry.aks@gmail.com>
2019-08-03 18:03:30 +0530

    Update Default State Object

--- ShoppingCart/front-end/src/store/reducers/DefaultState.js
+++ ShoppingCart/front-end/src/store/reducers/DefaultState.js
@@ -1,48 +1,66 @@
 export const initialState = {
-    products: {
-        productsList:[
-        {
-            id: "1234",
-            name: "Converse",
-            price: 50000,
-            currency: "Rs",
-            description: "Good Laptop",
-            rating: 3.5,
-            image: "images/converse.jpg"
-        },
-        {
-            id: "1235",
-            name: "Laptop",
-            price: 50000,
-            currency: "Rs",
-            description: "Good Laptop",
-            rating: 3.5,
-            image: "images/hp.jpg"
-        },
-        {
-            id: "1236",
-            name: "Cover",
-            price: 50000,
-            currency: "Rs",
-            description: "Good Laptop",
-            rating: 3.5,
-            image: "images/cover.jpg"
-        }
-        ]
-    },
-    cart: {
-        cartItems : [],
-        cartSummary : {
-            totalItems : 0,
-            totalAmount : 0,
-            shippingCharge : 0,
-            currency : 'INR'
-        }
-    },
-    apiStatus : {
-        loadingState : false,
-        apiStatus : 200,
-        message : ""
-    }
-};
+         products: {
+           productsList: [
+             {
+               id: "1234",
+               name: "Apple",
+               price: 50000,
+               currency: "Rs",
+               description: "Fresh Apples",
+               rating: 3.5,
+               image: "images/apple.jpg"
+             },
+             {
+               id: "1235",
+               name: "Banana",
+               price: 50000,
+               currency: "Rs",
+               description: "Fresh Bananas",
+               rating: 3.5,
+               image: "images/banana.jpg"
+             },
+             {
+               id: "1236",
+               name: "Orange",
+               price: 50000,
+               currency: "Rs",
+               description: "Fresh Oranges",
+               rating: 3.5,
+               image: "images/orange.jpg"
+             },
+             {
+               id: "1237",
+               name: "Peach",
+               price: 50000,
+               currency: "Rs",
+               description: "Fresh Peach",
+               rating: 3.5,
+               image: "images/peach.jpg"
+             },
+             {
+               id: "1237",
+               name: "Pear",
+               price: 50000,
+               currency: "Rs",
+               description: "Fresh Pear",
+               rating: 3.5,
+               image: "images/pear.jpg"
+             }
+           ]
+         },
+         cart: {
+           cartItems: [],
+           cartSummary: {
+             totalItems: 0,
+             totalAmount: 0,
+             shippingCharge: 0,
+             currency: "INR"
+           }
+         },
+         apiStatus: {
+           loadingState: false,
+           apiStatus: 200,
+           message: ""
+         }
+       };
 export default initialState;
