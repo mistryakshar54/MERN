af2b68b9fb5616d5ce1cedc4954e2557ec14994f
Akshar Mistry <mistry.aks@gmail.com>
2019-08-03 18:05:13 +0530

    Add Google Sign In method via Firebase

--- ShoppingCart/front-end/src/components/Layout/Header/Header.js
+++ ShoppingCart/front-end/src/components/Layout/Header/Header.js
@@ -2,22 +2,59 @@ import React from 'react';
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import './Header.scss';
+import { LinkContainer } from "react-router-bootstrap";
 
 import MiniCartComponent from '../../MiniCart/MiniCart';
+import {authProvider ,firebaseAuth} from '../../../firebaseConfig/FirebaseConfig';
+
 const AppHeader = ( props )=>{
+
+  const openLoginPopup = () => {
+      firebaseAuth()
+        .signInWithPopup(authProvider)
+        .then(function(result) {
+          // This gives you a Google Access Token. You can use it to access the Google API.
+          var token = result.credential.accessToken;
+          // The signed-in user info.
+          var user = result.user;
+          debugger;
+          console.log("Tokern" , token);
+          console.log("User", user);
+          // ...
+        })
+        .catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          debugger;
+          // ...
+        });
+  }
+
+
     return (
-      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-        <Navbar.Brand href="#home">Shoppie</Navbar.Brand>
+      <Navbar collapseOnSelect expand="lg" variant="light">
+        <Navbar.Brand>
+          <LinkContainer to="/">
+            <Nav.Link> Shoppie</Nav.Link>
+          </LinkContainer>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mr-auto"></Nav>
+          <Nav className="mr-auto" />
+          <Nav onClick={openLoginPopup}>Login</Nav>
           <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
+            <LinkContainer to="/checkout">
+              <Nav.Link>Checkout</Nav.Link>
+            </LinkContainer>
           </Nav>
           <Nav>
             <Nav.Link href="#">
-              <MiniCartComponent/>
-             
+              <MiniCartComponent />
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
