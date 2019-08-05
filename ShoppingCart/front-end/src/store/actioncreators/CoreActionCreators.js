0f51224932784dca2d084ade7b4bd818b964a3f1
Akshar Mistry <mistry.aks@gmail.com>
2019-08-05 23:28:15 +0530

    Add Actions for Dispatching Notifications

--- ShoppingCart/front-end/src/store/actioncreators/CoreActionCreators.js
+++ ShoppingCart/front-end/src/store/actioncreators/CoreActionCreators.js
@@ -1,6 +1,37 @@
-export const dispatchLoading = () => {};
-export const dispatchApiSuccess = () => {};
-export const dispatchApiError = () => {};
+
 export const dispatchPromise = ( endPoint , actionPayload ) => {
     console.log("Got a dispatch for following" , endPoint , actionPayload);
+}
+
+
+export const dispatchApiSuccess = () => {
+    return {
+      type: "SUCCESS_API"
+    };
+};
+
+export const dispatchApiError = ( payload ) => {
+    return {
+      type: "ERROR_API",
+      status: payload.status,
+      message: payload.message
+    };
+};
+
+export const dispatchApiLoading = ( ) => {
+    return {
+      type: "LOADING_API"
+    };
+};
+
+export const dispatchCloseAlertThunk = () => {
+    return dispatch => {
+        dispatch(resetApiStatus());
+    }
+}
+
+export const resetApiStatus = () => {
+    return {
+      type: "RESET_API"
+    };
 }
