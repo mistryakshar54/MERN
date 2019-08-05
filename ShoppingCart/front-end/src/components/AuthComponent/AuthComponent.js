8be4b5d9f08ffb78e14d5b4d2034b51490881c08
Akshar Mistry <mistry.aks@gmail.com>
2019-08-05 23:27:24 +0530

    Update Auth Component

--- ShoppingCart/front-end/src/components/AuthComponent/AuthComponent.js
+++ ShoppingCart/front-end/src/components/AuthComponent/AuthComponent.js
@@ -2,7 +2,8 @@ import React, { Component} from 'react';
 import {connect} from 'react-redux';
 import * as AuthActionCreators from '../../store/actioncreators/AuthActionCreator';
 import Nav from "react-bootstrap/Nav";
-
+import NavDropdown from "react-bootstrap/NavDropdown";
+import './AuthComponent.scss';
 
 class AuthComponent extends Component {
 
@@ -15,11 +16,31 @@ class AuthComponent extends Component {
           <React.Fragment>
             {this.props.authData.isAuthenticated === true ? (
               <React.Fragment>
-                <label>
-                    Logged in as {this.props.authData.authUser.given_name}
-                </label>
+                <img className="user-avatar"
+                  src={this.props.authData.authUser.picture}
+                  alt={this.props.authData.authUser.given_name}
+                />
+                <NavDropdown
+                  title={this.props.authData.authUser.given_name}
+                  id="basic-nav-dropdown"
+                >
+                  <NavDropdown.Item href="#action/3.1">
+                    Action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">
+                    Something
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+
                 <Nav.Link onClick={this.props.onLogoutHandler}>
-                    Logout
+                  Logout
                 </Nav.Link>
               </React.Fragment>
             ) : (
