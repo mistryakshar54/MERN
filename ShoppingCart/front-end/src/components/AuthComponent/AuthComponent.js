cc2978405968a12a299c4b2ce08ac05e7b6e5a7d
Akshar Mistry <mistry.aks@gmail.com>
2019-08-11 22:49:26 +0530

    Add routing functionality in Auth

--- ShoppingCart/front-end/src/components/AuthComponent/AuthComponent.js
+++ ShoppingCart/front-end/src/components/AuthComponent/AuthComponent.js
@@ -3,43 +3,47 @@ import {connect} from 'react-redux';
 import * as AuthActionCreators from '../../store/actioncreators/AuthActionCreator';
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { withRouter } from "react-router-dom";
 import './AuthComponent.scss';
 
 class AuthComponent extends Component {
-
-    componentDidMount(){
-        this.props.onCheckUserLoggedInHandler();
-    }
-    render(){
-        return (
+  componentDidMount() {
+    this.props.onCheckUserLoggedInHandler();
+  }
+  onRedirectToOrdersHandler = () => {
+    this.props.history.push("/orders");
+  };
+  render() {
+    return (
+      <React.Fragment>
+        {this.props.authData.isAuthenticated === true ? (
           <React.Fragment>
-            {this.props.authData.isAuthenticated === true ? (
-              <React.Fragment>
-                <img
-                  className="user-avatar"
-                  src={this.props.authData.authUser.picture}
-                  alt={this.props.authData.authUser.given_name}
-                />
-                <NavDropdown
-                  title={this.props.authData.authUser.given_name}
-                  id="basic-nav-dropdown"
-                >
-                  <NavDropdown.Item href="#">
-                    Orders
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#" onClick={this.props.onLogoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              </React.Fragment>
-            ) : (
-              <Nav.Link onClick={this.props.onLoginHandler}>
-                Login
-              </Nav.Link>
-            )}
+            <img
+              className="user-avatar"
+              src={this.props.authData.authUser.picture}
+              alt={this.props.authData.authUser.given_name}
+            />
+            <NavDropdown
+              title={this.props.authData.authUser.given_name}
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item
+                href="#"
+                onClick={this.onRedirectToOrdersHandler}
+              >
+                Orders
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#" onClick={this.props.onLogoutHandler}>
+                Logout
+              </NavDropdown.Item>
+            </NavDropdown>
           </React.Fragment>
-        );
-    }
+        ) : (
+          <Nav.Link onClick={this.props.onLoginHandler}>Login</Nav.Link>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 const mapStateToProps = ( state ) => {
@@ -53,11 +57,10 @@ const mapDispatchToProps = ( dispatch ) => {
     return{
         onLoginHandler : () => {dispatch(AuthActionCreators.openAuthPopupThunk())},
         onLogoutHandler : () => { dispatch(AuthActionCreators.logoutThunk()) },
-        onCheckIsAuthenticated : () => {},
         onCheckUserLoggedInHandler : () => { dispatch( AuthActionCreators.checkIsLoggedInThunk() ) }        
     }
 }
-export default connect(
+export default withRouter(connect(
   mapStateToProps,
   mapDispatchToProps
-)(AuthComponent);
+)(AuthComponent));
