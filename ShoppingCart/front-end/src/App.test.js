2423b8b5371697e4edaeb7cb74992b3def5b9ef8
Akshar Mistry <mistry.aks@gmail.com>
2019-09-01 19:54:23 +0530

    Add testing and snapshot files

--- ShoppingCart/front-end/src/App.test.js
+++ ShoppingCart/front-end/src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {shallow} from 'enzyme';
 import App from './App';
+import './setupTests';
+const app = shallow(<App/>);
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it('Renders App component correctly' , ()=>{
+    expect(app).toMatchSnapshot();
 });
