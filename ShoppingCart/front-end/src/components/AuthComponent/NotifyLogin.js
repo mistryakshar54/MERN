a1990d98acb3e0eaf46f6487ed102a984811ba22
Akshar Mistry <mistry.aks@gmail.com>
2019-09-04 17:06:29 +0530

    Update NotifyLogin.js

--- ShoppingCart/front-end/src/components/AuthComponent/NotifyLogin.js
+++ ShoppingCart/front-end/src/components/AuthComponent/NotifyLogin.js
@@ -3,41 +3,35 @@ import { connect } from "react-redux";
 import * as AuthActionCreators from "../../store/actioncreators/AuthActionCreator";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
-
+import "./NotifyLogin.scss";
 class NotifyLoginComponent extends Component {
   render() {
-      return (
-        <Col>
-          <Card>
-            <Card.Body>
-              <h2>You need to login before viewing this page.</h2>
-              <button
-                className="btn btn-danger"
-                onClick={() =>
-                  this.props.onLoginHandler(
-                    this.props.location.state.from.pathname || '/'
-                  )
-                }
-              >
-                Login
-              </button>
-            </Card.Body>
-          </Card>
-        </Col>
-      );
+    return (
+      <div className="container-fluid content-padding">
+        <h2>You need to login before viewing this page.</h2>
+        <button
+          className="btn btn-danger"
+          onClick={() =>
+            this.props.onLoginHandler(
+              this.props.location.state.from.pathname || "/"
+            )
+          }
+        >
+          Login
+        </button>
+      </div>
+    );
   }
 }
 
 const mapDispatchToProps = dispatch => {
   return {
-    onLoginHandler: (pathToRedirect) => {
+    onLoginHandler: pathToRedirect => {
       dispatch(AuthActionCreators.openAuthPopupThunk(pathToRedirect));
     }
   };
 };
-const mapStateToProps = state => {
-
-};
+const mapStateToProps = state => {};
 export default connect(
   mapStateToProps,
   mapDispatchToProps
