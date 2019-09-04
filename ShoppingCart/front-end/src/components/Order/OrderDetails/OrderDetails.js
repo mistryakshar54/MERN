40f2749530bf81c13c4c21077391e0b1ea7e303a
Akshar Mistry <mistry.aks@gmail.com>
2019-09-04 17:07:42 +0530

    Integerate Order Details using Loader

--- ShoppingCart/front-end/src/components/Order/OrderDetails/OrderDetails.js
+++ ShoppingCart/front-end/src/components/Order/OrderDetails/OrderDetails.js
@@ -4,18 +4,17 @@ import CardDeck from "react-bootstrap/CardDeck";
 import Table from "react-bootstrap/Table";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
-
+import * as OrderActionCreator from "../../../store/actioncreators/OrderActionCreator";
+import Loader from '../../Layout/Loader/Loader';
 class OrderDetailsComponent extends Component {
-  render() {
-    //TODO: Add check for spinner and empty data!!
-    if (this.props.coreData.loading === true) {
-      return <h1>Loading Data</h1>;
-    }
-    else if( !this.props.orderObject && !this.props.orderObject.currentOrder)
+  componentDidMount()
+  {
+    if(this.props.coreData.loadingState === false && !this.props.orderObject.currentOrder.orderitems)
     {
-        return <h1>No data received</h1>;
+      this.props.onShowOrderDetails(this.props.match.params.orderId);
     }
-    else {
+  }
+  render() { 
       let { orderitems, paymentsummary } = this.props.orderObject.currentOrder;
       return (
         <React.Fragment>
@@ -24,66 +23,76 @@ class OrderDetailsComponent extends Component {
             <Col>
               <Card>
                 <Card.Body>
-                  <h2>Item Details</h2>
-                  <Table className="text-centered" responsive>
-                    <tbody>
-                      {orderitems.map((item, index) => {
-                        return (
-                          <tr key={"ci -" + index}>
-                            <td>{index + 1}</td>
-                            <td>{item.name}</td>
-                            <td>
-                              {item.price} {item.currency}
+                  <Loader dataLoaded={orderitems ? true : false}>
+                    <h2>Item Details</h2>
+                    {orderitems ? (
+                      <Table className="text-centered" responsive>
+                        <tbody>
+                          {orderitems.map((item, index) => {
+                            return (
+                              <tr key={"ci -" + index}>
+                                <td>{index + 1}</td>
+                                <td>{item.name}</td>
+                                <td>
+                                  {item.price} {item.currency}
+                                </td>
+                                <td>{item.qty}</td>
+                                <td>
+                                  {item.price * item.qty} {item.currency}
+                                </td>
+                              </tr>
+                            );
+                          })}
+                          <tr>
+                            <td colSpan="4" className="text-right">
+                              Total
                             </td>
-                            <td>{item.qty}</td>
-                            <td>
-                              {item.price * item.qty} {item.currency}
-                            </td>
+                            <td>{paymentsummary.totalAmount} Rs </td>
                           </tr>
-                        );
-                      })}
-                      <tr>
-                        <td colSpan="4" className="text-right">
-                          Total
-                        </td>
-                        <td>{paymentsummary.totalAmount} Rs </td>
-                      </tr>
-                    </tbody>
-                  </Table>
+                        </tbody>
+                      </Table>
+                    ) : null}
+                  </Loader>
                 </Card.Body>
               </Card>
             </Col>
             <Col xs lg="4">
               <Card>
-                <Card.Body>
-                  <h2>Summary</h2>
-                  <div className="col-12">
-                    <label className="col-lg-4 text-left"> Amount </label>
-                    <label className="col-lg-4 text-right">
-                      {" "}
-                      {paymentsummary.totalAmount} Rs{" "}
-                    </label>
-                  </div>
-                  <div className="col-12">
-                    <label className="col-lg-4 text-left">
-                      Shipping Charge{" "}
-                    </label>
-                    <label className="col-lg-4 text-right">0 Rs </label>
-                  </div>
-                  <div className="col-12">
-                    <hr />
-                    <label className="col-lg-4 text-left">Final Amount </label>
-                    <label className="col-lg-4 text-right">
-                      {paymentsummary.totalAmount} Rs{" "}
-                    </label>
-                  </div>
-                </Card.Body>
+                  <Card.Body>
+                  <loader dataLoaded={paymentsummary ? true : false}>
+                    {paymentsummary ? (
+                      <div>
+                        <h2>Summary</h2>
+                        <div className="col-12">
+                          <label className="col-lg-4 text-left"> Amount </label>
+                          <label className="col-lg-4 text-right">
+                            {paymentsummary.totalAmount} Rs{" "}
+                          </label>
+                        </div>
+                        <div className="col-12">
+                          <label className="col-lg-4 text-left">
+                            Shipping Charge{" "}
+                          </label>
+                          <label className="col-lg-4 text-right">0 Rs </label>
+                        </div>
+                        <div className="col-12">
+                          <hr />
+                          <label className="col-lg-4 text-left">
+                            Final Amount{" "}
+                          </label>
+                          <label className="col-lg-4 text-right">
+                            {paymentsummary.totalAmount} Rs{" "}
+                          </label>
+                        </div>
+                      </div>
+                    ) : null}
+                   </loader>
+                  </Card.Body>
               </Card>
             </Col>
           </CardDeck>
         </React.Fragment>
-      );
-    }
+      ); 
   }
 }
 
@@ -93,10 +102,15 @@ const mapStateToProps = state => {
     coreData: state.CoreReducer
   };
 };
-// const mapDispatchToProps = ( dispatch ) => {
-//     return{
+const mapDispatchToProps = ( dispatch ) => {
+    return {
+      onShowOrderDetails: orderId => {
+        dispatch(OrderActionCreator.fetchOrderDetailsThunk(orderId));
+      }
+    };
+}
 
-//     }
-// }
-
-export default connect(mapStateToProps)(OrderDetailsComponent);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(OrderDetailsComponent);
