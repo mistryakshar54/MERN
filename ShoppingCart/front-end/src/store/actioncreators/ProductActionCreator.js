d058b598768210fdb6934e9dea8944c8fd09a86d
Akshar Mistry <mistry.aks@gmail.com>
2019-09-09 13:11:38 +0530

    Add Test case for Product Action Creators

--- ShoppingCart/front-end/src/store/actioncreators/ProductActionCreator.js
+++ ShoppingCart/front-end/src/store/actioncreators/ProductActionCreator.js
@@ -1,39 +1,10 @@
 import * as CoreActions from "./CoreActionCreators";
-import { dispatchGET } from "./CoreActionCreators";
+
 export const fetchAllProductsThunk = () => {
-           return async (dispatch, getState) => {
-             dispatch(CoreActions.dispatchApiLoading());
-             let resp = await CoreActions.dispatchGET("products.json");
-             const productsList = [];
-             if (resp.data && Object.keys(resp.data).length > 0) {
-               for (var dataId in resp.data) {
-                 productsList.push(resp.data[dataId]);
-               }
-               dispatch(fetchAllProducts(productsList));
-               dispatch(CoreActions.dispatchApiSuccess());
-             } else {
-               let payload = {
-                 status: 404,
-                 message: "No Products Fetched"
-               };
-               dispatch(CoreActions.dispatchApiError(payload));
-             }
-           };
-         };
-
-export const fetchAllProducts = productData => {
-         return {
-           type: "FETCH_ALL_PRODUCTS",
-           productsList: productData
-         };
-       };
-
-export const fetchDataThunk = () => {
-  return async dispatch => {
-      
-      try {
-      const resp = await fetch("http://example.com/todos");
-      console.log(resp.body);
+  return async (dispatch, getState) => {
+    dispatch(CoreActions.dispatchApiLoading());
+    return await CoreActions.dispatchGET("products.json")
+    .then( resp => {
       const productsList = [];
       if (resp.data && Object.keys(resp.data).length > 0) {
         for (var dataId in resp.data) {
@@ -48,9 +19,20 @@ export const fetchDataThunk = () => {
         };
         dispatch(CoreActions.dispatchApiError(payload));
       }
-    }
-    catch (e) {
-      return dispatch(CoreActions.dispatchApiError());
-    }
-  }
-}
+    } )
+    .catch( err => {
+      let payload = {
+        status: 404,
+        message: "No Products Fetched"
+      };
+      dispatch(CoreActions.dispatchApiError(payload));
+    });
+  };
+};
+
+export const fetchAllProducts = productData => {
+  return {
+    type: "FETCH_ALL_PRODUCTS",
+    productsList: productData
+  };
+};
--- ShoppingCart/front-end/src/store/actioncreators/ProductActionCreator.test.js
+++ ShoppingCart/front-end/src/store/actioncreators/ProductActionCreator.test.js
@@ -1,13 +1,14 @@
 import thunk from "redux-thunk";
-import fetchMock from "fetch-mock";
+import instance from "../../axiosConfig/axiosconfig";
 import * as ProductActions from './ProductActionCreator';
 import "../../setupTests";
 import configureMockStore from "redux-mock-store";
 import expect from "expect";
+import moxios from 'moxios';
+import initialState from '../reducers/DefaultState';
 
 const middleware = [thunk];
 const mockStore = configureMockStore(middleware);
-
 const productList = [
              {
                id: "1234",
@@ -20,38 +21,71 @@ const productList = [
              }]
 
 describe('Test ProductActions' , () => {
+    let store;
+      beforeEach(() => {
+        moxios.install(instance);
+         store = mockStore(initialState);
+      });
+      afterEach(() => {
+        moxios.uninstall();
+      });
 
-    it("Should return FetchAllProducts Action" , () => {
-        const mockActionResponse = {
-          type: "FETCH_ALL_PRODUCTS",
-          productsList: productList
-        };
-        expect(ProductActions.fetchAllProducts(productList)).toEqual(
-          mockActionResponse
-        );
-    });
-
-    it("Mock FetchAllProductsThunk", () => {
-      fetchMock.getOnce("http://example.com/todos", [{"currency":"Rs","description":"Good Sofa","image":"images/sofa.jpg","name":"Sofa","price":50000,"productId":"671458","rating":4},{"currency":"Rs","description":"Comfirtable Studytable","image":"images/studytable.jpg","name":"Study Table","price":30000,"productId":"671459","rating":3.5},{"currency":"Rs","description":"Queen Size Good bed","image":"images/queen_size_bed.jpg","name":"Bed","price":100000,"productId":"671460","rating":4.5},{"currency":"Rs","description":"Wardrobe with wooden touch","image":"images/wardrobe.jpg","name":"Wardrobe","price":50000,"productId":"671461","rating":3},{"currency":"Rs","description":"Confi Recliner","image":"images/recliner.jpg","name":"Recliner","price":40000,"productId":"671462","rating":3.5}] 
-      );
-
-        const store = mockStore({
-          productList : [
-             {
-               id: "1234",
-               name: "Sofa",
-               price: 50000,
-               currency: "Rs",
-               description: "Fresh Apples",
-               rating: 3.5,
-               image: "images/sofa.jpg"
-             }]
+      it("Loads all products correctly", done => {
+        moxios.wait(function() {
+          let request = moxios.requests.mostRecent();
+          request.respondWith({
+            status: 200,
+            response: productList
+          });
         });
 
-     return store.dispatch( ProductActions.fetchDataThunk() ).then(() => {
+        const expectedActions = [
+          {
+            type: "LOADING_API"
+          },
+          {
+            type: "FETCH_ALL_PRODUCTS",
+            productsList: productList
+          },
+          {
+            type: "SUCCESS_API"
+          }
+        ];
+        return store
+          .dispatch(ProductActions.fetchAllProductsThunk())
+          .then(() => {
+            const actualAction = store.getActions();
+            expect(actualAction).toEqual(expectedActions);
+            done();
+          });
+      });
 
-         console.log("Store Actions: ", store.getActions());
-     });
-    });
+      it("Throws Error when no Products are fetched!", done => {
+        moxios.wait(function() {
+          let request = moxios.requests.mostRecent();
+          request.respondWith({
+            status: 200,
+            response: []
+          });
+        });
+
+        const expectedActions = [
+          {
+            type: "LOADING_API"
+          },
+          {
+            type: "ERROR_API",
+            status: 404,
+            message: "No Products Fetched"
+          }
+        ];
+        return store
+          .dispatch(ProductActions.fetchAllProductsThunk())
+          .then(() => {
+            const actualAction = store.getActions();
+            expect(actualAction).toEqual(expectedActions);
+            done();
+          });
+      });
 
 });
