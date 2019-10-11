a6d3326467dd196d51a0e3cdd6ac0a7543a731c2
Akshar Mistry <mistry.aks@gmail.com>
2019-10-11 22:13:46 +0530

    comment out temp cookie

--- ShoppingCart/back-end/index.js
+++ ShoppingCart/back-end/index.js
@@ -6,7 +6,16 @@ var indexRouter = require('./routes/index')
 const productRouter = require('./routes/product');
 const userRouter = require("./routes/user");
 const bodyParser = require('body-parser');
-var session = require("express-session");
+
+const session = require("express-session");
+var MongoDBStore = require("connect-mongodb-session")(session);
+const MONGODB_URL ="mongodb+srv://aksharadmin:aksharadmin@node-app-cluster-x1adf.mongodb.net/shop?retryWrites=true&w=majority";
+
+// var MongoStore = new MongoDBStore({
+//   uri : MONGODB_URL,
+//   collection:'sessions'
+// });
+
 
 app.use(bodyParser.json());
 app.use(
@@ -16,23 +25,25 @@ app.use(
 );
 
 app.set("trust proxy", 1); // trust first proxy
-app.use(
-  session({
-    secret: "keyboard cat",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true }
-  })
-);
+// app.use(
+//   session({
+//     secret: "keyboard cat",
+//     resave: false,
+//     saveUninitialized: false,
+//     store:MongoStore
+//   })
+// );
 app.use('/', indexRouter);
 app.use('/product', productRouter);
-app.use("/login", userRouter);
+app.use("/user", userRouter);
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-mongoose.connect( "mongodb+srv://aksharadmin:aksharadmin@node-app-cluster-x1adf.mongodb.net/shop?retryWrites=true&w=majority" )
-.then( resp => {
+mongoose
+  .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(resp => {
     app.listen(4000);
-}).catch(error => {
-    console.log("Error connecting using Mongoose" , error);
-})
+  })
+  .catch(error => {
+    console.log("Error connecting using Mongoose", error);
+  });
