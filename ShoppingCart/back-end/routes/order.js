0124c395571325c8ef77f3e1799d29722e846e1a
Akshar Mistry <mistry.aks@gmail.com>
2019-10-17 22:36:36 +0530

    Add route paths for order

--- ShoppingCart/back-end/routes/order.js
+++ ShoppingCart/back-end/routes/order.js
@@ -2,5 +2,11 @@ const express = require('express');
 const orderRouter = express.Router();
 const orderController = require('../controllers/OrderController');
 
-orderRouter.get('/' , orderController.createOrder);
+orderRouter.get('/' ,orderController.getAllOrders);
+orderRouter.get("/user/:userId", orderController.getAllUserOrders);
+orderRouter.get("/:orderId", orderController.getOrderDetails);
+orderRouter.post("/", orderController.createOrder);
+orderRouter.put("/:orderId", orderController.getAllUserOrders);
+orderRouter.delete("/:orderId", orderController.deleteOrder);
+
 module.exports = orderRouter;
