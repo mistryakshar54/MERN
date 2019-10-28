2a465ccaef3a50ae0460d103996786e9f9153e31
Akshar Mistry <mistry.aks@gmail.com>
2019-10-28 20:59:58 +0530

    Update Loader.js

--- ShoppingCart/shopping-cart-admin-app/src/Components/AppLayout/Loader/Loader.js
+++ ShoppingCart/shopping-cart-admin-app/src/Components/AppLayout/Loader/Loader.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { useSelector , shallowEqual } from "react-redux";
 const Loader = ( props ) => {
     const coreData =  useSelector( state => state.CoreReducer ,  shallowEqual);
-    debugger;
-    console.log("coreData", coreData.loadingState);
-   
-        if(coreData.loadingState){
-            return <div>Loaded!</div>
-        }
-        else{
-            return props.children;
-        }
+    if(coreData.loadingState){
+        return <div>Loaded!</div>
+    }
+    else{
+        return props.children;
+    }
         
 }
 export default Loader;
